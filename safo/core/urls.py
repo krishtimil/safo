1a298a279e397381008a5ab6fe24264e4001556f
Bindu Paudel <106421905+bigya01@users.noreply.github.com>
2023-01-09 23:28:43 +0545

    fix:improved homepage UI

--- safo/core/urls.py
+++ safo/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.homepage, name="home"),
     path('contact-us/', views.contactpage, name="contact"),
 ]
+urlpatterns += staticfiles_urlpatterns()
