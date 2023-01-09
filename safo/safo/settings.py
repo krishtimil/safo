be1af58155601a7d1c5742d56c2fce249a0bfa29
krishtimil <30183049+krishtimil@users.noreply.github.com>
2023-01-09 22:05:56 +0545

    fix: fixed static sites

--- safo/safo/settings.py
+++ safo/safo/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
